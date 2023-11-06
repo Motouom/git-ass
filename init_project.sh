fcf2976f8ae4fd9a2c7359c3ffc8de8bc1a12137
Motouom <149515567+Motouom@users.noreply.github.com>
2023-11-06 12:48:20 +0100

    Update init_project.sh

--- init_project.sh
+++ init_project.sh
@@ -10,9 +10,60 @@ while true; do
     
     
     if [[ -z $folder_name ]]; then
-        echo "Folder name cannot be empty. Please try again."
+        echo "Folder name cannot be empty"
         continue
     fi
+
     
     mkdir  ~/Desktop/$folder_name
+	
     echo "Folder '$folder_name' created on the Desktop'!"
+
+
+
+	echo "What is your project type?
+	1. python
+	2. bash
+	"
+
+	read response
+
+	#making a choice
+	case $response in
+		1)
+
+			#entering into the users folder
+			cd ~/Desktop/$folder_name
+
+			#installing python3 virtual env
+			pip install virtualenv
+
+			#creating a new virtual env
+			echo "what is the name of the virtual environment you want to creat"
+			read env_name
+			virtualenv $env_name
+
+			#activating the virtual environmet
+			source $env_name/bin/activate
+			;;
+
+		2)
+
+			cd ~/Desktop/$folder_name
+
+			touch README
+		;;
+	esac
+	#asking the user if he wants to carry out a new project.
+	echo "do you want to work on a new project (yes/no)?"
+	read answer
+	case $answer in 
+		yes)
+			continue
+	;;
+		no)
+			break
+	;;
+		esac
+
+done
