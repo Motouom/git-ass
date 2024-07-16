e7dc94ee4de49428d44e8e87bafd0096d73550e2
Victoire <motouomvictor@gmail.com>
2024-07-16 07:51:39 +0100

    added a dockerfile for the frontend modified second frr

--- packit-backend/src/main/kotlin/com/packit/packit_backend/PackitBackendApplication.kt
+++ packit-backend/src/main/kotlin/com/packit/packit_backend/PackitBackendApplication.kt
@@ -1,14 +1,10 @@
 package com.packit.packit_backend
 
-import org.springframework.boot.Configuration
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
-@Configuration
-public class PackitBackendApplication {
-
-}
-
+@SpringBootApplication
+class PackitBackendApplication
 
 fun main(args: Array<String>) {
     runApplication<PackitBackendApplication>(*args)
